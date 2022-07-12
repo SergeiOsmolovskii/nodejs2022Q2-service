19e991fdefeca035b9ec4c3ce664b537b3fb4563
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-07-12 21:01:23 +0300

    feat: add basic swagger doc and .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
 import { AppModule } from './app.module';
+import { parse } from 'yaml';
+import { dirname, join } from 'path';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+  
+  SwaggerModule.setup('doc', app, document);
+  
+  
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
