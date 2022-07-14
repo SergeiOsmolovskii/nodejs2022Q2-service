5b981831af818e22fe1df2237b6b477e01bad8d6
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-07-15 00:08:16 +0300

    feat: add user endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ArtistModule } from './modules/artist/artist.module';
 import { AlbumModule } from './modules/album/album.module';
 import { TrackModule } from './modules/track/track.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ArtistModule, AlbumModule, TrackModule],
+  imports: [ArtistModule, AlbumModule, TrackModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { IUser } from '../user.interface';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { v4 as uuid } from 'uuid';
+import { UpdatePasswordDto } from '../dto/update-password.tdo';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly users: IUser[] = [];
+
+  public async getAllUsers(): Promise<IUser[]> {
+    return this.users;
+  }
+
+  public async createUser(user: CreateUserDto): Promise<IUser> {
+    const createdAt = +Date.now();
+    const updatedAt = +Date.now();
+    const version = 1;
+    const newUser = {
+      login: user.login,
+      id: uuid(),
+      createdAt: createdAt,
+      version: version,
+      updatedAt: updatedAt,
+    };
+
+    this.users.push({...newUser, password: user.password});
+    return newUser;
+  }
+
+  public async getUserById(id: string): Promise<IUser> {
+
+    const user = this.users.find(user => user.id === id);
+   
+    const currentData = {
+      login: user.login,
+      id: user.id,
+      createdAt: user.createdAt,
+      version: user.version,
+      updatedAt: user.updatedAt
+    };    
+
+    return currentData;
+  }
+
+  public async updateUser(id: string, passwords: UpdatePasswordDto): Promise<IUser> {
+    const index = this.users.findIndex(user => user.id === id);
+    const currentUser = this.users[index];
+
+    if (currentUser.password !== passwords.oldPassword) {
+      throw new ForbiddenException('Old password is incorrect');
+    }
+
+    const updatedUser = {
+      login: currentUser.login,
+      createdAt: currentUser.createdAt,
+      id,
+      version: currentUser.version + 1,
+      updatedAt: +Date.now()
+    };
+
+    this.users[index] = {
+      ...updatedUser,
+      password: passwords.newPassword
+    };
+    
+    return updatedUser;
+  }
+
+  public async deleteUser(id: string): Promise<IUser> {
+    const index = this.users.findIndex(user => user.id === id);
+    const user = this.users[index];
+    this.users.splice(index, 1);
+    return user;
+  }
+
+}
